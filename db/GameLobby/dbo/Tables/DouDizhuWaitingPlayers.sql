f8bba2f518cfe70af83df6330301a0b42022971e
Yushu Hsiao <yushuhsiao@gmail.com>
2017-04-19 07:07:01 +0800

    .

---
+++ db/GameLobby/dbo/Tables/DouDizhuWaitingPlayers.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[DouDizhuWaitingPlayers] (
+    [id]        BIGINT           NOT NULL,
+    [_ver]      ROWVERSION       NOT NULL,
+    [Lobby]     UNIQUEIDENTIFIER NOT NULL,
+    [GameId]    INT              NOT NULL,
+    [PlayerId]  INT              NOT NULL,
+    [ConfigId]  INT              NOT NULL,
+    [JoinTime]  DATETIME         CONSTRAINT [DF_DouDizhuWaitingPlayers_JoinTime] DEFAULT (getdate()) NOT NULL,
+    [CreatorId] INT              NULL,
+    [Password]  VARCHAR (20)     NULL,
+    [Account]   VARCHAR (50)     NULL,
+    [Nickname]  NVARCHAR (50)    NULL,
+    CONSTRAINT [PK_DouDizhuWaitingPlayers] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [IX_DouDizhuWaitingPlayers] UNIQUE NONCLUSTERED ([Lobby] ASC, [GameId] ASC, [PlayerId] ASC)
+);
+
